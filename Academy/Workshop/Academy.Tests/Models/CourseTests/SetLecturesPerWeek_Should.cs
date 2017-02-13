13476406c4161988a08109bffab58fc983161856
AnnaYanakieva <a.ianakieva@gmail.com>
2017-02-13 16:50:39 +0200

    Added tests

---
+++ Academy/Workshop/Academy.Tests/Models/CourseTests/SetLecturesPerWeek_Should.cs
@@ -0,0 +1,25 @@
+﻿using Academy.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy.Tests.Models.CourseTests
+{
+    [TestFixture]
+    public class SetLecturesPerWeek_Should
+    {
+        [TestCase(0)]
+        [TestCase(8)]
+        public void ThrowsArgumentException_WhenInvalidValueIsPassed(int lecturesPerWeek)
+        {
+            //Arrange
+            var course = new Course("Valid name", 5, new DateTime(2017, 02, 10), new DateTime(2017, 03, 10));
+
+            //Act & Assert
+            Assert.Throws<ArgumentException>(() => course.LecturesPerWeek = lecturesPerWeek);
+        }
+    }
+}
---
+++ Academy/Workshop/Academy.Tests/Models/CourseTests/ToString_Should.cs
@@ -0,0 +1,69 @@
+﻿using Academy.Models;
+using Academy.Models.Contracts;
+using Moq;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academy.Tests.Models.CourseTests
+{
+    [TestFixture]
+    public class ToString_Should
+    {
+        [Test]
+        public void IterateOverCollection_WhenCollectionIsNotEmpty()
+        {
+            //Arrange
+            var course = new Course("Valid name", 5, new DateTime(2017, 02, 10), new DateTime(2017, 03, 10));
+
+            var lectureMock = new Mock<ILecture>();
+
+            lectureMock.Setup(x => x.ToString());
+
+            course.Lectures.Add(lectureMock.Object);
+
+            //Act
+            course.ToString();
+
+            //Assert
+            lectureMock.Verify(x => x.ToString(), Times.Once);
+        }
+
+        [Test]
+        public void IterateOverCollection_WhenCollectionIsNotEmpty_JustMock()
+        {
+            //Arrange
+            var course = new Course("Valid name", 5, new DateTime(2017, 02, 10), new DateTime(2017, 03, 10));
+
+            var lectureMock = Telerik.JustMock.Mock.Create<ILecture>();
+
+            Telerik.JustMock.Mock.Arrange(() => lectureMock.ToString());
+
+            course.Lectures.Add(lectureMock);
+
+            //Act
+            course.ToString();
+
+            //Assert
+            Telerik.JustMock.Mock.Assert(() => lectureMock.ToString(), Telerik.JustMock.Occurs.Once());
+          //  lectureMock.Verify(x => x.ToString(), Times.Once);
+        }
+
+        [Test]
+        public void AddMessageToEndResult_WhenCollectionEmpty()
+        {
+            //Arrange
+            var course = new Course("Valid name", 5, new DateTime(2017, 02, 10), new DateTime(2017, 03, 10));
+
+           
+            //Act
+            var result = course.ToString();
+
+            //Assert
+            StringAssert.Contains("no lectures", result);
+        }
+    }
+}
